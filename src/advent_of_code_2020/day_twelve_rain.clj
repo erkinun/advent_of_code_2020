90f654a01612ffba1f15adbe58df08cebcfc1216
Erkin Unlu <erkin.unlu@rvu.co.uk>
2020-12-13 16:39:58 +0000

    day twelve part 2

--- src/advent_of_code_2020/day_twelve_rain.clj
+++ src/advent_of_code_2020/day_twelve_rain.clj
@@ -11,12 +11,20 @@
                          :r :west}
                  })
 
-(def status (atom {:facing :east
-                   :x      0                                ;; west/east
-                   :y      0                                ;; north/south
-                   }))
+(def status (atom {:facing   :east
+                   :x        0                              ;; west/east
+                   :y        0                              ;; north/south
+                   :waypoint {:x 10
+                              :y 1}}))
 
-(defn forward [value]
+(defn forward [times]
+  (let [x-mov (* times (get-in @status [:waypoint :x]))
+        y-mov (* times (get-in @status [:waypoint :y]))]
+    (swap! status (fn [m] (-> m
+                              (update :x + x-mov)
+                              (update :y + y-mov))))))
+
+(defn forward-part-1 [value]
   (let [direction (:facing @status)]
     (condp = direction
       :east (swap! status update-in [:x] + value)
@@ -24,7 +32,25 @@
       :west (swap! status update-in [:x] - value)
       :north (swap! status update-in [:y] + value))))
 
+(defn turn-wp [{:keys [x y]} direction]
+  (let [x-dir     (if (pos-int? x) :east :west)
+        y-dir     (if (pos-int? y) :north :south)
+        new-y-dir (-> directions x-dir direction)
+        new-x-dir (-> directions y-dir direction)
+        abs-y (Math/abs y)
+        abs-x (Math/abs x)]
+    {:x (if (= :east new-x-dir) abs-y (- 0 abs-y))
+     :y (if (= :north new-y-dir) abs-x (- 0 abs-x))}))
+
 (defn turn [direction value]
+  (let [times (/ value 90)]
+    (loop [t      times
+           cur-wp (:waypoint @status)]
+      (if (= t 0)
+        (swap! status assoc :waypoint cur-wp)
+        (recur (dec t) (turn-wp cur-wp direction))))))
+
+(defn turn-part-1 [direction value]
   (let [times  (/ value 90)
         facing (:facing @status)]
     (loop [t          times
@@ -37,10 +63,10 @@
   (let [cmd   (subs inst-str 0 1)
         value (Integer/parseInt (subs inst-str 1))]
     (condp = cmd
-      "N" (swap! status update-in [:y] + value)
-      "S" (swap! status update-in [:y] - value)
-      "E" (swap! status update-in [:x] + value)
-      "W" (swap! status update-in [:x] - value)
+      "N" (swap! status update-in [:waypoint :y] + value)
+      "S" (swap! status update-in [:waypoint :y] - value)
+      "E" (swap! status update-in [:waypoint :x] + value)
+      "W" (swap! status update-in [:waypoint :x] - value)
       "F" (forward value)
       "R" (turn :r value)
       "L" (turn :l value))
@@ -77,4 +103,11 @@
   (process-instruction "R90")
   (manhattan)
   (navigate test-instructions)
-  (navigate instructions))
+  (navigate instructions)
+  (turn-wp {:x 10 :y 4} :r)
+  (turn-wp {:x 10 :y 4} :l)
+  (-> {:x 10 :y 4}
+      (turn-wp :l)
+      (turn-wp :l))
+  (turn-wp {:x -4 :y 10} :l)
+  )
